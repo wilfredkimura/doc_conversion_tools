aa5e0720a2517b9fc4541bf7623b474965dacb46
wilfredkimura <wilfredmutahi9@gmail.com>
2026-05-08 21:01:41 +0300

    feat: prepared the application for a Native Render Deployment by removing the Docker dependencies and adapting the server to handle Render's dynamic environment.

--- app/main.py
+++ app/main.py
@@ -188,4 +188,5 @@ async def open_folder(request: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
